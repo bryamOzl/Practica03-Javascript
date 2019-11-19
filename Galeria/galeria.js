6466110eb7aaf88c1c651d6eb2111c3e0b98eea1
bryamOzl <bryan95_19@hotmail.com>
2019-11-19 13:56:23 -0500

    Practica03-Javascript

--- Galeria/galeria.js
+++ Galeria/galeria.js
@@ -7,15 +7,20 @@ array = [{ "id": 1, "imagen": "img/img1.jpg" },
          { "id": 7, "imagen": "img/img7.jpg" },
          { "id": 8, "imagen": "img/img8.jpg" },
          { "id": 9, "imagen": "img/img9.jpg" },
-         { "id": 10, "imagen": "img/img10.jpg" }];
-         
+         { "id": 10, "imagen": "img/img10.jpg"},
+         { "id": 11, "imagen": "img/img11.jpg" },
+         { "id": 12, "imagen": "img/img12.jpg" },
+         { "id": 13, "imagen": "img/img13.jpg" },
+         { "id": 14, "imagen": "img/img14.jpg" },
+         { "id": 15, "imagen": "img/img15.jpg" }];
+
 imagenes = [1, 2, 3, 4, 5];
 
 var pActual = 0;
 
 function inicio() {
     for (i = 0; i < 5; i++) {
-        imagenes[i] = Math.floor(Math.random() * (10));
+        imagenes[i] = Math.floor(Math.random() * (15));
     }
     pActual = 0;
     verificar();
