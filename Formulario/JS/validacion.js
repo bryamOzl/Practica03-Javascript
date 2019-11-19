125940a99e0b9ffbe24a8429070efeacfdc062fa
bryamOzl <bryan95_19@hotmail.com>
2019-11-18 21:24:44 -0500

    Practica03-Javascript

--- Formulario/JS/validacion.js
+++ Formulario/JS/validacion.js
@@ -7,6 +7,20 @@ function validarCamposObligatorios()
         if(elemento.value == '' && elemento.type == 'text'){
             if(elemento.id == 'cedula'){
                 document.getElementById('mensajeCedula').innerHTML = '<br>La cedula esta vacia'
+            }else if (elemento.id == 'nombres') {
+                document.getElementById('mensajeNombres').innerHTML = '<br>Los nombres estan vacíos'
+            } else if (elemento.id == 'apellidos') {
+                document.getElementById('mensajeApellidos').innerHTML = '<br>Los apellidos estan vacíos'
+            } else if (elemento.id == 'direccion') {
+                document.getElementById('mensajeDireccion').innerHTML = '<br>La dirección esta vacía'
+            } else if (elemento.id == 'telefono') {
+                document.getElementById('mensajeTelefono').innerHTML = '<br>El telefono está vacío'
+            } else if (elemento.id == 'fechaNacimiento') {
+                document.getElementById('mensajeFechaNacimiento').innerHTML = '<br>La fecha de nacimiento esta vacía'
+            } else if (elemento.id == 'correo') {
+                document.getElementById('mensajeCorreo').innerHTML = '<br>El correo esta vacío'
+            } else if (elemento.id == 'contrasena') {
+                document.getElementById('mensajeContrasena').innerHTML = '<br>La contraseña esta vacía'
             }
 
             elemento.style.border = '1px red solid'
